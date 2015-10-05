4de585d9318110df49515e68d1fc73a7b83d2aed
staff-netpartner <staff@netpartner.jp>
2015-10-05 14:20:27 +0900

    Update render.js

--- render.js
+++ render.js
@@ -2,4 +2,4 @@ var url = location.href;
 var renderer = new SpreadsheetRenderer({
   key: '1T-yHSNh7sv3Dfkar3k9DZqwqDBev1Q-_9EdVaB492AU',
   template:'<li><a href="%2%">%1%</a></li>' });
-renderer.render('select * WHERE A = "' + (url) + '" AND D = 1', 'result');
+renderer.render('select * WHERE A = "' + (url) + '" AND D = "on"', 'result');
